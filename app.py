fd499751063e5ed0d8a41c8c8a11ef6a711021a2
Chaitrali Awasare <chaitraliawasare@Chaitralis-MacBook-Air.local>
2026-03-11 12:08:09 +0530

    Adding Healthchecks and Alerts

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from flask import Flask
+
 import psycopg2
 import socket
+from prometheus_client import Counter, Histogram, generate_latest
 
 app = Flask(__name__)
+
+REQUEST_COUNT = Counter(
+    "flask_http_requests_total",
+    "Total HTTP requests",
+    ["method", "endpoint"]
+)
+
+REQUEST_LATENCY = Histogram(
+    "flask_request_latency_seconds",
+    "Request latency"
+)
 
 @app.route("/")
 def hello():
@@ -23,6 +36,17 @@ def check_db_connection():
         return "Database connection successful"
     except Exception as e:
         return f"Database connection failed: {e}"
+
+def hello_world():
+     REQUEST_COUNT.labels(method="GET", endpoint="/").inc()
+
+     with REQUEST_LATENCY.time():
+         hostname = socket.gethostname()
+         return f"{check_db_connection()} from {hostname}"
+@app.route("/metrics")
+def metrics():
+    return generate_latest(), 200, {"Content-Type": "text/plain"}
+        
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
